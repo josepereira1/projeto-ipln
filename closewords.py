9c9826bc5dbb8f5c896bae3961ecbbc6509a1d1f
Ricardo Petronilho <ricardopetronilho@Ricardos-Air.lan>
2019-10-09 19:07:47 +0100

    tp1 "bruto" feito, falta meter um UI mas "pipi" e a configuração de after e before

--- closewords.py
+++ closewords.py
@@ -5,25 +5,41 @@ import sys
 
 filename = sys.argv[1]
 word = sys.argv[2]
-number = sys.argv[3]
+number = str(int(sys.argv[3])-1) # our algorithm iterates till last word
 
-text = open(filename).read()
+text = open(filename).read() # reads all file to string
+regex = "(" + word + "(?:[\ \t\n\,\;\:\"\'\[\]\(\)]+\w+(?:(?:[\-\@\.\w]*)\w+)?){1," + number + "})"
+lines = re.findall(regex, text) # applies regex to string
 
-#regex = "("+word+"(?:[\ \t\n\,\;\:\"\'\[\]\(\)\&]+\w+(?:(?:\-|[\@\.\w]*)\w+)?){1,"+number+"})"
+dic = {} # init dic
 
-output = re.findall(regex, text)
+for line in lines:
+	
+	# remove 'trash' from string
+	clean = re.sub('[\n\,\;\:\"\'\[\]\(\)]+', '', line).replace('\t',' ')
+	# splits by space
+	words = clean.split(' ')
+	# first word is never used, ex: 'está'
+	words.pop(0)
+	
+	key = ""
+	i = 0
+	for word in words:
+		
+		# increment key:
+		# words = ['um', 'lindo', 'dia'] 
+		# ex: key = um
+		#     key = um lindo 
+		#     key = um lindo dia
+		if key == "": key = words[i]
+		else: key = key + " " + words[i]
+		i = i + 1 
+		
+		# increment number of hits
+		if key in dic: dic[key] = dic[key] + 1	
+		else: dic[key] = 1	
 
-print(output)
 
-#cona = ""
-
-# for elem in output:
-#  	for ch in elem:
-#  		if ch != ' ':
-#  			cona = cona + ch
-
-
-# print(cona)
-
-
-
+# sort alphabetically Hash Table by key
+for key in sorted(dic.keys()):
+	print(key, dic[key])
